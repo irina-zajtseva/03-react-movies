55f401902f05a4a50137115a78593a993f43d64c
Irina Zaitseva <ira.zajtseva89@gmail.com>
2026-02-07 16:23:41 +0200

    fixed folder services

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { fetchMovies } from "../../servises/movieService";
+import { fetchMovies } from "../../services/movieService";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import SearchBar from "../SearchBar/SearchBar";
 import "./App.module.css";
