d43c6096501c7b10b9215e1749da82d163fa97cf
Irina Zaitseva <ira.zajtseva89@gmail.com>
2026-02-07 16:04:01 +0200

    fixed bug in token

--- src/servises/movieService.ts
+++ src/servises/movieService.ts
@@ -1,26 +1,32 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
+
 const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 interface FetchMovies {
-    results: Movie[]
+  results: Movie[];
 }
 
 export async function fetchMovies(query: string): Promise<Movie[]> {
-  const options = {
-    method: "GET",
-    url: "https://api.themoviedb.org/3/search/movie",
-    params: {
-      query: query,
-      include_adult: false,
-      language: "en-US",
-      page: 1,
-    },
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${TMDB_TOKEN}`,
-    },
-  };
-  const {data} = await axios.request<FetchMovies>(options);
+  if (!TMDB_TOKEN) {
+    throw new Error("TMDB token is missing: VITE_TMDB_TOKEN is undefined");
+  }
+
+  const { data } = await axios.get<FetchMovies>(
+    "https://api.themoviedb.org/3/search/movie",
+    {
+      params: {
+        query,
+        include_adult: false,
+        language: "en-US",
+        page: 1,
+      },
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${TMDB_TOKEN}`,
+      },
+    }
+  );
+
   return data.results;
 }
